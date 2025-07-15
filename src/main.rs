7b5d35fd43b03a2743d04290a1005fd8fe80330f
cowkami <lee.unseo17@gmail.com>
2025-07-15 14:35:11 +0900

    define premitive board

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use std::fmt;
+use std::fmt::{self, write};
+
+const BOARD_SIZE: usize = 9;
 
 fn main() {
-    let stone = Stone::Black;
-    println!("{:}", stone);
-    println!("{:}", Stone::White);
+    let board = Board::new();
+    println!("{}", board);
 }
 
-#[derive(Debug)]
+#[derive(Copy, Clone)]
 enum Stone {
     Black,
     White,
@@ -22,5 +23,60 @@ impl fmt::Display for Stone {
                 write!(f, "⚪️")
             }
         }
+    }
+}
+
+struct Board {
+    stones: [[Option<Stone>; BOARD_SIZE]; BOARD_SIZE],
+}
+
+impl Board {
+    pub fn new() -> Board {
+        Board {
+            stones: [[None; BOARD_SIZE]; BOARD_SIZE],
+        }
+    }
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // upper line
+        write!(f, "┌")?;
+        write!(
+            f,
+            "{}",
+            (0..BOARD_SIZE)
+                .into_iter()
+                .map(|_| "──")
+                .collect::<String>()
+        )?;
+        write!(f, "┐\n")?;
+
+        for i in 0..BOARD_SIZE {
+            // left side line
+            write!(f, "│")?;
+            for j in 0..BOARD_SIZE {
+                if let Some(stone) = self.stones[i][j] {
+                    write!(f, "{}", stone)?;
+                } else {
+                    write!(f, "＋")?;
+                }
+            }
+            // right side line
+            write!(f, "│\n")?;
+        }
+
+        // lower line
+        write!(f, "└")?;
+        write!(
+            f,
+            "{}",
+            (0..BOARD_SIZE)
+                .into_iter()
+                .map(|_| "──")
+                .collect::<String>()
+        )?;
+        write!(f, "┘\n")?;
+        Ok(())
     }
 }
