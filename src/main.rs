e5ef4aa5c9be9573ef69c07f4abadabd9dd5f463
cowkami <lee.unseo17@gmail.com>
2025-07-16 12:39:06 +0900

    draw rich board

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use std::fmt::{self, write};
+use std::fmt;
 
 const BOARD_SIZE: usize = 9;
+const BLACK: &str = "○";
+const WHITE: &str = "●";
 
 fn main() {
-    let board = Board::new();
+    let mut board = Board::new();
+    board.stones[4][4] = Some(Stone::Black);
+    board.stones[4][5] = Some(Stone::White);
+    board.stones[4][3] = Some(Stone::White);
+    board.stones[3][3] = Some(Stone::White);
+    board.stones[0][0] = Some(Stone::White);
     println!("{}", board);
 }
 
@@ -17,10 +24,10 @@ impl fmt::Display for Stone {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Stone::Black => {
-                write!(f, "⚫")
+                write!(f, "{}", BLACK)
             }
             Stone::White => {
-                write!(f, "⚪️")
+                write!(f, "{}", WHITE)
             }
         }
     }
@@ -45,35 +52,67 @@ impl fmt::Display for Board {
         write!(
             f,
             "{}",
-            (0..BOARD_SIZE)
+            (0..BOARD_SIZE * 2 + 5)
                 .into_iter()
-                .map(|_| "──")
+                .map(|_| "─")
                 .collect::<String>()
         )?;
         write!(f, "┐\n")?;
 
+        // upper coordination
+        write!(f, "│  ")?;
+        for i in 0..BOARD_SIZE {
+            write!(f, " {}", i + 1)?;
+        }
+        write!(f, "   │\n")?;
+
         for i in 0..BOARD_SIZE {
             // left side line
-            write!(f, "│")?;
+            write!(f, "│ {} ", i + 1)?;
             for j in 0..BOARD_SIZE {
                 if let Some(stone) = self.stones[i][j] {
-                    write!(f, "{}", stone)?;
+                    write!(f, "{} ", stone)?;
                 } else {
-                    write!(f, "＋")?;
+                    if i == 0 && j == 0 {
+                        write!(f, "{}", "┌─")?;
+                    } else if i == 0 && j == BOARD_SIZE - 1 {
+                        write!(f, "{}", "┐")?;
+                    } else if i == BOARD_SIZE - 1 && j == 0 {
+                        write!(f, "└─")?;
+                    } else if i == BOARD_SIZE - 1 && j == BOARD_SIZE - 1 {
+                        write!(f, "┘")?;
+                    } else if i == 0 && j != 0 {
+                        write!(f, "{}", "┬─")?;
+                    } else if i == BOARD_SIZE - 1 && j != 0 {
+                        write!(f, "{}", "┴─")?;
+                    } else if i != 0 && j == 0 {
+                        write!(f, "{}", "├─")?;
+                    } else if i != 0 && j == BOARD_SIZE - 1 {
+                        write!(f, "{}", "┤")?;
+                    } else {
+                        write!(f, "{}", "┼─")?;
+                    }
                 }
             }
             // right side line
-            write!(f, "│\n")?;
+            write!(f, " {} │\n", i + 1)?;
         }
+
+        // lower coordination
+        write!(f, "│  ")?;
+        for i in 0..BOARD_SIZE {
+            write!(f, " {}", i + 1)?;
+        }
+        write!(f, "   │\n")?;
 
         // lower line
         write!(f, "└")?;
         write!(
             f,
             "{}",
-            (0..BOARD_SIZE)
+            (0..BOARD_SIZE * 2 + 5)
                 .into_iter()
-                .map(|_| "──")
+                .map(|_| "─")
                 .collect::<String>()
         )?;
         write!(f, "┘\n")?;
