7e79baceefac283d1f1d9afb629ff73281402592
cowkami <lee.unseo17@gmail.com>
2025-07-16 16:33:58 +0900

    draw hoshi on board

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ use std::fmt;
 const BOARD_SIZE: usize = 9;
 const BLACK: &str = "○";
 const WHITE: &str = "●";
+const NUMBERS: [char; 19] = [
+    '①', '②', '③', '④', '⑤', '⑥', '⑦', '⑧', '⑨', '⑩', //
+    '⑪', '⑫', '⑬', '⑭', '⑮', '⑯', '⑰', '⑱', '⑲',
+];
 
 fn main() {
     let mut board = Board::new();
@@ -62,19 +66,49 @@ impl fmt::Display for Board {
         // upper coordination
         write!(f, "│  ")?;
         for i in 0..BOARD_SIZE {
-            write!(f, " {}", i + 1)?;
+            write!(f, " {}", NUMBERS[i])?;
         }
         write!(f, "   │\n")?;
 
         for i in 0..BOARD_SIZE {
             // left side line
-            write!(f, "│ {} ", i + 1)?;
+            write!(f, "│ {} ", NUMBERS[i])?;
             for j in 0..BOARD_SIZE {
                 if let Some(stone) = self.stones[i][j] {
                     write!(f, "{} ", stone)?;
                 } else {
                     if i == 0 && j == 0 {
                         write!(f, "{}", "┌─")?;
+                    // draw hoshi
+                    } else if BOARD_SIZE == 9
+                        && (
+                            // draw hoshi at sumi
+                            (i == 2 && j == 2)
+                            || (i == 2 && j == BOARD_SIZE - 3)
+                            || (i == BOARD_SIZE - 3 && j == 2)
+                            || (i == BOARD_SIZE - 3 && j == BOARD_SIZE - 3)
+                            // draw tengen
+                            || (i == BOARD_SIZE / 2 && j == BOARD_SIZE / 2)
+                        )
+                    {
+                        write!(f, "•─")?;
+                    } else if (BOARD_SIZE == 13 || BOARD_SIZE == 19)
+                        && (
+                            // draw hoshi at sumi
+                            (i == 3 && j == 3)
+                            || (i == 3 && j == BOARD_SIZE - 4)
+                            || (i == BOARD_SIZE - 4 && j == 3)
+                            || (i == BOARD_SIZE - 4 && j == BOARD_SIZE - 4)
+                            // draw tengen
+                            || (i == BOARD_SIZE / 2 && j == BOARD_SIZE / 2)
+                            // drow hoshi at hen
+                            || (i == 3 && j == BOARD_SIZE / 2)
+                            || (i == BOARD_SIZE / 2 && j == 3)
+                            || (i == BOARD_SIZE - 4 && j == BOARD_SIZE / 2)
+                            || (i == BOARD_SIZE / 2 && j == BOARD_SIZE - 4)
+                        )
+                    {
+                        write!(f, "•─")?;
                     } else if i == 0 && j == BOARD_SIZE - 1 {
                         write!(f, "{}", "┐")?;
                     } else if i == BOARD_SIZE - 1 && j == 0 {
@@ -95,13 +129,13 @@ impl fmt::Display for Board {
                 }
             }
             // right side line
-            write!(f, " {} │\n", i + 1)?;
+            write!(f, " {} │\n", NUMBERS[i])?;
         }
 
         // lower coordination
         write!(f, "│  ")?;
         for i in 0..BOARD_SIZE {
-            write!(f, " {}", i + 1)?;
+            write!(f, " {}", NUMBERS[i])?;
         }
         write!(f, "   │\n")?;
 
